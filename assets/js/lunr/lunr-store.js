87f8c89021a300edd7fc43a568883d2aed452fed
Varabho Bhikkhu <tudong@sumedharama.pt>
2025-09-06 07:13:59 +0200

    sa

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,166 @@
+/*
+Copyright (c) 2013-2020 Michael Rose and contributors
+MIT License
+From minimal-mistakes: https://github.com/mmistakes/minimal-mistakes/
+*/
+var store = [{
+    "title": "Archivo de Noticias",
+    "subtitle": "",
+    "excerpt": "Archive of this theme",
+    "content": "",
+      "categories": null,
+      "date": null,
+      "tags": null,
+      "url": "/archive",
+      "img": null
+    },{
+    "title": "Como llegar",
+    "subtitle": "",
+    "excerpt": "Archive of this theme",
+    "content": "L’Aragall La masia L’Aragall se encuentra en: Camí L’Aragall S/N, 08591 Aiguafreda - Barcelona Mapa e indicaciones En coche: Dirección: Camí de L’Aragall, Masia de L’Aragall, Aiguafreda. 08591 (Barcelona) Indicaciones:...",
+      "categories": null,
+      "date": null,
+      "tags": null,
+      "url": "/direccion",
+      "img": null
+    },{
+    "title": "Mantenimiento",
+    "subtitle": "",
+    "excerpt": "",
+    "content": "Calendario de mantenimiento Calendario de visitantes Manuales de usuario Máquina de Coser Singer5802 Lavadoras LG ThinQ 9/13Kg Lavavajillas Bosch Serie4 SilencePlus Corta Césped Impresora HP DeskJet2823e Consumo de agua Noticias...",
+      "categories": null,
+      "date": null,
+      "tags": null,
+      "url": "/mantenimiento",
+      "img": null
+    },{
+    "title": "La Masía",
+    "subtitle": "Historia de antaño",
+    "excerpt": "L’Aragall 1956 - 1968",
+    "content": "L’Aragall 1956 - 1968 Compra de la propiedad en 1956 Nosotros cada año íbamos a Martinet y luego mi padre subía cada fin de semana por la carretera de Vic-Ripoll-Collada...",
+      "categories": ["masía"],
+      "date": "1960-05-06",
+      "tags": ["masía","historia","image"],
+      "url": "/19600506_narracion-masia/",
+      "img": "/assets/images/arco.jpg"
+    },{
+    "title": "Ceremonia de Apertura",
+    "subtitle": "Día de inauguración",
+    "excerpt": "Ceremonia de Apertura",
+    "content": "Ceremonia de Apertura   La ceremonia de apertura esta oficiada por Achan Amaro, abad del monasterio Amaravati en Inglaterra.   Ceremonia de apertura",
+      "categories": ["sangha"],
+      "date": "2024-10-09",
+      "tags": ["sangha"],
+      "url": "/20241009_ceremonia-apertura/",
+      "img": "https://i.imgur.com/Y3fAr6q.jpg"
+    },{
+    "title": "Visitas desde Portugal",
+    "subtitle": "Nuestros hermanos nos visitan",
+    "excerpt": "Visitas desde Portugal",
+    "content": "Visitas desde Portugal   visitas portugal",
+      "categories": ["sangha"],
+      "date": "2025-04-06",
+      "tags": ["sangha"],
+      "url": "/20250406_visitas-portugal/",
+      "img": "https://i.imgur.com/l0y7ujF.jpg"
+    },{
+    "title": "Caminante no hay camino",
+    "subtitle": "Descripcion del tudong 2025 Malaga Pannagiri",
+    "excerpt": "",
+    "content": "Caminante no hay camino Proverbios y cantares (XXIX) Caminante, son tus huellas el camino y nada más; Caminante, no hay camino, se hace camino al andar. Al andar se hace...",
+      "categories": ["sangha"],
+      "date": "2025-05-06",
+      "tags": ["sangha","tudong"],
+      "url": "/20250506_caminante-no-hay-camino/",
+      "img": "/assets/images/banderitas.jpg"
+    },{
+    "title": "Asalha Puja 2025",
+    "subtitle": "Día de entrada en el periodo de lluvias",
+    "excerpt": "Asalha",
+    "content": "Asalha Asalha Puja, también conocido como el Día del Dharma, es uno de los festivales más importantes del budismo Theravada, ya que celebra el primer sermón de Buddha, el Sermón...",
+      "categories": ["sangha"],
+      "date": "2025-07-06",
+      "tags": ["sangha"],
+      "url": "/20250706_asalha-2025/",
+      "img": "https://i.imgur.com/radOxFI.jpg"
+    },{
+    "title": "Preparando chimenea para buddha y horno",
+    "subtitle": "Demolición de la chimenea",
+    "excerpt": "Demolición de la chimenea",
+    "content": "Demolición de la chimenea   Preparando espacio para Buddha Rupa.   Inminente instalación de hornos de leña   chimenea y hornos de leña",
+      "categories": ["mantenimiento"],
+      "date": "2025-07-06",
+      "tags": ["mantenimiento","interior"],
+      "url": "/20250706_demolicion-chimenea/",
+      "img": "https://i.imgur.com/3AXu6Zn.jpg"
+    },{
+    "title": "Reconstrucción muro",
+    "subtitle": "Reconstrucción del muro exterior",
+    "excerpt": "Reconstrucción del muro",
+    "content": "Reconstrucción del muro Sr.Manuel y Sr.Francisco son actualmente nuestros maestros de obra. Muro Técnica a imitar En este video, un señor explica como es la técnica para hacer muros secos....",
+      "categories": ["mantenimiento"],
+      "date": "2025-07-06",
+      "tags": ["mantenimiento","exterior"],
+      "url": "/20250706_reconstruyendo-muro/",
+      "img": "https://i.imgur.com/1YugDqf.jpg"
+    },{
+    "title": "Reparación de encimera",
+    "subtitle": "Encimera podrida, Achan Nanadassano la repara",
+    "excerpt": "Reparación de encimera",
+    "content": "Reparación de encimera La encimera está podrida y en proceso de reparación. Sentimos mucho las inconveniencias que pueda causar, pero sino cambiamos la madera se va a venir todo para...",
+      "categories": ["mantenimiento"],
+      "date": "2025-07-06",
+      "tags": ["mantenimiento","interior"],
+      "url": "/20250706_reparacion-encimera/",
+      "img": "https://i.imgur.com/ZmGqtDq.jpg"
+    },{
+    "title": "Visita a Montserrat",
+    "subtitle": "Nos llevan de excursion a Montserrat",
+    "excerpt": "Nos llevan a Montserrat",
+    "content": "Nos llevan a Montserrat   Gracias Anna y Pep celebrando su 20 aniversario.   Visita Montserrat 2025-08-04",
+      "categories": ["sangha"],
+      "date": "2025-08-04",
+      "tags": ["sangha"],
+      "url": "/20250804_visita-montserrat/",
+      "img": "https://upload.wikimedia.org/wikipedia/commons/c/cd/Montserrat_des_de_Manresa.JPG"
+    },{
+    "title": "Clases de thailandes",
+    "subtitle": "Kru Pharanee nos enseña",
+    "excerpt": "Clases de Thailandes",
+    "content": "Clases de Thailandes   Gracias Maestra por su ayuda.",
+      "categories": ["sangha"],
+      "date": "2025-08-06",
+      "tags": ["sangha"],
+      "url": "/20250806_clases-de-thailandes/",
+      "img": "https://i.imgur.com/kC3JE3C.jpg"
+    },{
+    "title": "Visita a Tagamanent",
+    "subtitle": "Nos llevan a tagamanent",
+    "excerpt": "Nos llevan a Tagamanent",
+    "content": "Nos llevan a Tagamanent Se han encontrado restos que confirman que hubo asentamientos ibéricos en el enclave del castillo. El castillo fue documentado por los cronistas medievales por primera vez...",
+      "categories": ["sangha"],
+      "date": "2025-08-12",
+      "tags": ["sangha"],
+      "url": "/20250812_visita-tagamanent/",
+      "img": "https://upload.wikimedia.org/wikipedia/commons/9/95/Castell_Tagamanent-0594sh.jpg"
+    },{
+    "title": "Aguas fecales",
+    "subtitle": "Nos explican el sistema de aguas fecales o fosa séptica",
+    "excerpt": "Fosa Septica",
+    "content": "Fosa Septica El Sr Rosendo Matas nos hizo una visita y nos explicó amablemente todos los entresijos del canal y el deposito que instalaron en 2016. Con anterioridad, las aguas...",
+      "categories": ["mantenimiento"],
+      "date": "2025-08-27",
+      "tags": ["mantenimiento","exterior"],
+      "url": "/20250827_aguas_fecales/",
+      "img": "https://organicabiotech.com/wp-content/uploads/2021/03/Septic-Tank-Blog-Img.jpg.webp"
+    },{
+    "title": "Adquisición Caravana",
+    "subtitle": "Nos llega la caravana",
+    "excerpt": "Hoy Miércoles adquirimos para el monasterio una caravana por 1100Euros + 200Euros para traerla.Ahora, queda limpiarla, despejarla de mobiliario y ponerle aislante para el invierno.",
+    "content": "Hoy Miércoles adquirimos para el monasterio una caravana por 1100Euros + 200Euros para traerla. Ahora, queda limpiarla, despejarla de mobiliario y ponerle aislante para el invierno.   Caravana",
+      "categories": [],
+      "date": "2025-08-28",
+      "tags": ["exterior"],
+      "url": "/20250828_adquisicion-caravana/",
+      "img": "https://upload.wikimedia.org/wikipedia/commons/c/c8/Caravan.jpg"
+    }]
